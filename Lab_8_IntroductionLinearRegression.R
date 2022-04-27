50f2bf5a4e639ac6cbcab080b0acf53d88f66e57
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-04-27 10:10:16 +0100

    Adding contents from Lab 8

---
+++ Lab_8_IntroductionLinearRegression.R
@@ -0,0 +1,7 @@
+# Author:       Niall McGuinness
+# Title:        Labs - Introduction to Linear Regression
+# Description:  Illustrating use of cor and lm functions for linear regression 
+# Date:         04/2022
+# Version:      1.0
+# See:          http://htmlpreview.github.io/?https://github.com/andrewpbray/oiLabs-base-R/blob/master/simple_regression/simple_regression.html
+
