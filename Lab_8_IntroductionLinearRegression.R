c83a2e6eae1e533dbff41b5bb301637e8f33ddaf
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-05-04 10:34:56 +0100

    Added lab code from Wk 11 classs

--- Lab_8_IntroductionLinearRegression.R
+++ Lab_8_IntroductionLinearRegression.R
@@ -5,3 +5,20 @@
 # Version:      1.0
 # See:          http://htmlpreview.github.io/?https://github.com/andrewpbray/oiLabs-base-R/blob/master/simple_regression/simple_regression.html
 
+download.file("http://www.openintro.org/stat/data/mlb11.RData", destfile = "mlb11.RData")
+load("mlb11.RData")
+
+# correlation co-efficient
+cor(mlb11$runs, mlb11$at_bats)
+
+# plotting scatterplot
+plot(x = mlb11$at_bats, y = mlb11$runs)
+
+# generate linear regression statistics
+m1 <- lm(runs ~ at_bats, data = mlb11)
+
+# display key linear model/regression statistics
+summary(m1)
+
+plot(mlb11$runs ~ mlb11$at_bats)
+abline(m1)
