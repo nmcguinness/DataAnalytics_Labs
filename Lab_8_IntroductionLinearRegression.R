6daaa96e56ae2464d0b316cf766db8491a124c1d
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-05-06 08:21:25 +0100

    Added QQ plot

--- Lab_8_IntroductionLinearRegression.R
+++ Lab_8_IntroductionLinearRegression.R
@@ -22,3 +22,11 @@ summary(m1)
 
 plot(mlb11$runs ~ mlb11$at_bats)
 abline(m1)
+
+plot(m1$residuals ~ mlb11$at_bats)
+abline(h = 0, lty = 3)  # adds a horizontal dashed line at y = 0
+
+hist(m1$residuals)
+
+qqnorm(m1$residuals)
+qqline(m1$residuals)  # adds diagonal line to the normal prob plot
